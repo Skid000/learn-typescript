bfdb37cb59d67e2e9427d8ee43a266dcd5abecf9
hmmm <no@no.no>
2022-09-10 16:09:03 -0600

    formatting

--- index.ts
+++ index.ts
@@ -1,17 +1,5 @@
-// Welcome to
-// __________         __    __  .__                               __
-// \______   \_____ _/  |__/  |_|  |   ____   ______ ____ _____  |  | __ ____
-//  |    |  _/\__  \\   __\   __\  | _/ __ \ /  ___//    \\__  \ |  |/ // __ \
-//  |    |   \ / __ \|  |  |  | |  |_\  ___/ \___ \|   |  \/ __ \|    <\  ___/
-//  |________/(______/__|  |__| |____/\_____>______>___|__(______/__|__\\_____>
-//
-// This file can be a nice home for your Battlesnake logic and helper functions.
-//
-// To get you started we've included code to prevent your Battlesnake from moving backwards.
-// For more info see docs.battlesnake.com
-
-import runServer from './server';
-import { GameState, InfoResponse, MoveResponse } from './types';
+import runServer from './gamefiles/server';
+import { GameState, InfoResponse, MoveResponse } from './gamefiles/types';
 
 // info is called when you create your Battlesnake on play.battlesnake.com
 // and controls your Battlesnake's appearance
