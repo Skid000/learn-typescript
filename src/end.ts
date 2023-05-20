74065a17c672c9ed4112c67120dad0f4640994e0
Your Name <you@example.com>
2023-05-19 20:50:42 -0600

    more changes

--- src/end.ts
+++ src/end.ts
@@ -3,6 +3,6 @@ import { Shared } from './persistence/shared';
 import { GameState } from './types';
 export function end(gameState: GameState, sharedMap: Map<string, Shared>,pop: Population): void {
     pop.score(sharedMap.get(gameState.game.id)?.member,gameState.turn,gameState.you.length,gameState);
+    console.log(`Game ended at ${gameState.turn}!\nWith Fitness of ${sharedMap.get(gameState.game.id)?.member.fitness}\n`);
     sharedMap.delete(gameState.game.id);
-    console.log(`Game ended at ${gameState.turn}!`);
 }
