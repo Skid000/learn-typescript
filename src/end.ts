84d6818114fe151384669bdb58c54641c158726a
Your Name <you@example.com>
2023-05-20 10:30:29 -0600

    uploading more gens

--- src/end.ts
+++ src/end.ts
@@ -4,6 +4,6 @@ import { GameState } from './types';
 export function end(gameState: GameState, sharedMap: Map<string, Shared>,pop: Population): void {
     let mem = sharedMap.get(gameState.game.id)?.member;
     pop.score(mem,gameState.turn,gameState.you.length,gameState);
-    console.log(`Game ended at ${gameState.turn}!\nWith Fitness of ${mem?.fitness}\nParams: ${mem == undefined || Object.keys(mem.param).map(e=>`${e}: ${mem?.param[e]}`).join('\n')}`);
+    console.log(`=========\nGame ended at ${gameState.turn}!\nWith Fitness of ${mem?.fitness}\nParams: ${mem == undefined || Object.keys(mem.param).map(e=>`${e == 'a' ? '' : '\t'}${e}: ${mem?.param[e]}`).join('\n')}\n=========`);
     sharedMap.delete(gameState.game.id);
 }
